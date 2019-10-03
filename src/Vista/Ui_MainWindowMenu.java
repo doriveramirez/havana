5150cef0a9f75c99cb3c61845940e58086b063ae
RichardVinueza <44475931+RichardVinueza@users.noreply.github.com>
2019-10-03 17:16:39 +0100

    1.2

---
+++ src/Vista/Ui_MainWindowMenu.java
@@ -0,0 +1,7 @@
+/********************************************************************************
+ ** Form generated from reading ui file 'tareaHotelMenu.jui'
+ **
+ ** Created by: Qt User Interface Compiler version 4.8.7
+ **
+ ** WARNING! All changes made in this file will be lost when recompiling ui file!
+ ********************************************************************************/
